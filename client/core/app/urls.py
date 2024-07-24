a7712dbb148cd0e8e95a39641afbb16371de38a7
VrushaliAPoojary <vrushaliash04@gmail.com>
2024-07-25 00:20:08 +0530

    pratham p shetty

--- client/core/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import FileUploadView, FileListView
-
-urlpatterns = [
-    path('api/upload/', FileUploadView.as_view(), name='file-upload'),
-    path('api/files/', FileListView.as_view(), name='file-list'),
-]
---
+++ server/core/app/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .models import File
+from .serializers import FileSerializer
+
+
+class FileUploadView(generics.CreateAPIView):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+
+class FileListView(generics.ListAPIView):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
