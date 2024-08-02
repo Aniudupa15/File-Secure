9120e1fd2e3cefc0e9d6d2964f430df512058167
Aniudupa15 <aniudupa15@gmail.com>
2024-08-02 20:33:57 +0530

    Revert "completed"
    This reverts commit af46bafdfaaa126298a468dab62eae737e8a0efc.

--- server/core/app/models.py
+++ server/core/app/models.py
@@ -1,24 +1,8 @@
 from django.db import models
-from cryptography.fernet import Fernet
-import os
 
+# Create your models here.
 class File(models.Model):
     file = models.FileField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        # Encrypt file before saving
-        if self.file and not self._state.adding:
-            key = os.getenv('ENCRYPTION_KEY')
-            cipher = Fernet(key)
-            encrypted_data = cipher.encrypt(self.file.read())
-            self.file.save(self.file.name, ContentFile(encrypted_data), save=False)
-        super().save(*args, **kwargs)
-
-    def get_decrypted_file(self):
-        # Decrypt file before returning
-        key = os.getenv('ENCRYPTION_KEY')
-        cipher = Fernet(key)
-        self.file.seek(0)  # Ensure we're at the start of the file
-        decrypted_data = cipher.decrypt(self.file.read())
-        return decrypted_data
+    
