53814e9fb7660b18180dadc6b22154442485e5b5
PrathamPShetty <prathampshetty99sai@gmail.com>
2024-08-03 23:40:45 +0530

    file

--- server/core/app/urls.py
+++ server/core/app/urls.py
@@ -1,10 +1,10 @@
 # app/urls.py
 
 from django.urls import path
-from .views import FileUploadView, FileListView, CreateEncryptedFile
+from .views import FileUploadView, FileListView
 
 urlpatterns = [
     path('api/upload/', FileUploadView.as_view(), name='file-upload'),
     path('api/files/', FileListView.as_view(), name='file-list'),
-    path('api/upload/encrypted/', CreateEncryptedFile.as_view(), name='encrypted-file-upload'),
+    #path('api/upload/encrypted/', CreateEncryptedFile.as_view(), name='encrypted-file-upload'),
 ]
--- server/core/app/views.py
+++ server/core/app/views.py
@@ -3,6 +3,8 @@
 from rest_framework import generics
 from .models import File
 from .serializers import FileSerializer
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 class FileUploadView(generics.CreateAPIView):
     queryset = File.objects.all()
@@ -12,33 +14,3 @@ class FileListView(generics.ListAPIView):
     queryset = File.objects.all()
     serializer_class = FileSerializer
 
-from django.core.files.uploadhandler import MemoryFileUploadHandler, TemporaryFileUploadHandler
-from django.views.generic.edit import CreateView
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt, csrf_protect
-from .models import File
-
-class EncryptedFileUploadHandler(MemoryFileUploadHandler):
-    # Implement the custom file upload handler logic here
-    pass
-
-@method_decorator(csrf_exempt, name='dispatch')
-class CreateEncryptedFile(CreateView):
-    model = File
-    fields = ["file"]
-
-    def post(self, request, *args, **kwargs):
-        request.upload_handlers = [
-            EncryptedFileUploadHandler(request=request),
-            MemoryFileUploadHandler(request=request),
-            TemporaryFileUploadHandler(request=request)
-        ]  
-        return self._post(request)
-
-    @method_decorator(csrf_protect)
-    def _post(self, request):
-        form = self.get_form()
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
